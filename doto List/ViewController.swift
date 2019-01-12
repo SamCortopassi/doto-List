fdf78fcad34e4e824fc256360c7ccff856bd2e65
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-11 21:46:50 -0600

    Initial Commit

---
+++ doto List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  doto List
+//
+//  Created by Samantha Cortopassi on 1/11/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
