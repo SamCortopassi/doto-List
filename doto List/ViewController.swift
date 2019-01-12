be8c620941a31a6e22603f6f6059eec8b6d5178b
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-11 21:54:52 -0600

    IBOutlets

--- doto List/ViewController.swift
+++ doto List/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func addButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
